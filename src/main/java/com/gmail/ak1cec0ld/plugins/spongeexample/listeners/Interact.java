b6d229fd5ca45b3b022dd4155aa5424715bcac1a
Jeremy <1cew1r3@gmail.com>
2020-07-06 16:25:17 -0700

    fails to build due to player

--- src/main/java/com/gmail/ak1cec0ld/plugins/spongeexample/listeners/Interact.java
+++ src/main/java/com/gmail/ak1cec0ld/plugins/spongeexample/listeners/Interact.java
@@ -2,6 +2,9 @@ package com.gmail.ak1cec0ld.plugins.spongeexample.listeners;
 
 import com.gmail.ak1cec0ld.plugins.spongeexample.SpongeExample;
 import com.pixelmonmod.pixelmon.Pixelmon;
+import com.pixelmonmod.pixelmon.storage.PlayerPartyStorage;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.common.ForgeHooks;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.InteractBlockEvent;
@@ -18,9 +21,10 @@ public class Interact {
     public void onInteract(InteractBlockEvent event){
         instance.getLogger().info(event.getTargetBlock().getState().getType().toString());
 
-        //EntityPlayerMP player;
-        //PlayerPartyStorage storage = Pixelmon.storageManager.getParty(player);
+        EntityPlayerMP player;
+        PlayerPartyStorage storage = Pixelmon.storageManager.getParty(player);
         if(Pixelmon.isServer())
+            ForgeHooks.getCraftingPlayer();
             return;
     }
 
